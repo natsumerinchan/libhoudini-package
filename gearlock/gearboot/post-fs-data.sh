349967978a87c1f9ab80bed9e2556b939e675a6f
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-04-15 13:26:02 +0800

    Update post-fs-data.sh

--- gearlock/gearboot/post-fs-data.sh
+++ gearlock/gearboot/post-fs-data.sh
@@ -3,6 +3,7 @@
 # You must delete this if you dont need it.
 
 # Define your props inside a var in <prop> <value> format
+
 ARM_TRANSLATION="
 ro.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
 ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
@@ -11,7 +12,9 @@ ro.dalvik.vm.native.bridge libhoudini.so
 ro.enable.native.bridge.exec 1
 ro.dalvik.vm.isa.arm x86
 ro.dalvik.vm.isa.arm64 x86_64
+ro.zygote zygote64_32
 "
 
 # Now call the var over gearprop with quotes
+
 gearprop "$ARM_TRANSLATION"
