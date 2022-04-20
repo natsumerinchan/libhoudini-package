1d1dcc768ed672f120c1167ad396aa97a7b972b8
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-04-20 14:58:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ if test "$SYSTEM_ARCH" != "x86_64"; then
 fi
 
 # Before install
-read -rn1 -p "$(geco "++++ Are you sure to install this package? ? [y/${GREEN}N${RC}]") " c
+read -rn1 -p "$(geco "++++ Do you sure you want to install this package? ? [y/${GREEN}N${RC}]") " c
 test "${c,,}" != 'y' && exit 101 #(exit-code ref: https://wiki.supreme-gamers.com/gearlock/developer-guide/#install-sh-exit-code)
 
 # Delete the original libhoudini
