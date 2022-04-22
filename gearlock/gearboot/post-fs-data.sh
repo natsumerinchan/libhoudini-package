9a6677e6ad1bbd275bcf4dd4e712a07a2ea0263a
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-04-22 09:27:47 +0800

    Update post-fs-data.sh

--- gearlock/gearboot/post-fs-data.sh
+++ gearlock/gearboot/post-fs-data.sh
@@ -8,8 +8,15 @@ ARM_TRANSLATION="
 ro.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
 ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
 ro.product.cpu.abilist64 x86_64,arm64-v8a
+ro.vendor.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
+ro.vendor.product.cpu.abilist32 x86,armeabi-v7a,armeabi
+ro.vendor.product.cpu.abilist64 x86_64,arm64-v8a
+ro.odm.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
+ro.odm.product.cpu.abilist32 x86,armeabi-v7a,armeabi
+ro.odm.product.cpu.abilist64 x86_64,arm64-v8a
 ro.dalvik.vm.native.bridge libhoudini.so
 ro.enable.native.bridge.exec 1
+ro.enable.native.bridge.exec64 1
 ro.dalvik.vm.isa.arm x86
 ro.dalvik.vm.isa.arm64 x86_64
 ro.zygote zygote64_32
