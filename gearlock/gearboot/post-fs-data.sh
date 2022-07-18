62f224b93149d089d91dbb1cf99196830d4c75fb
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-18 17:52:10 +0800

    Update post-fs-data.sh

--- gearlock/gearboot/post-fs-data.sh
+++ gearlock/gearboot/post-fs-data.sh
@@ -4,20 +4,7 @@
 
 # Define your props inside a var in <prop> <value> format
 
-ARM_TRANSLATION_x86="
-ro.product.cpu.abilist x86,armeabi-v7a,armeabi
-ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
-ro.vendor.product.cpu.abilist x86,armeabi-v7a,armeabi
-ro.vendor.product.cpu.abilist32 x86,armeabi-v7a,armeabi
-ro.odm.product.cpu.abilist x86,armeabi-v7a,armeabi
-ro.odm.product.cpu.abilist32 x86,armeabi-v7a,armeabi
-ro.dalvik.vm.native.bridge libhoudini.so
-ro.enable.native.bridge.exec 1
-ro.dalvik.vm.isa.arm x86
-ro.zygote zygote32
-"
-
-ARM_TRANSLATION_x86_64="
+ARM_TRANSLATION="
 ro.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
 ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
 ro.product.cpu.abilist64 x86_64,arm64-v8a
@@ -36,6 +23,5 @@ ro.zygote zygote64_32
 "
 
 # Now call the var over gearprop with quotes
-# You should modify 'gearprop "$ARM_TRANSLATION_x86_64"' to 'gearprop "$ARM_TRANSLATION_x86"' if your package architecture is x86.
 
-gearprop "$ARM_TRANSLATION_x86_64"
+gearprop "$ARM_TRANSLATION"
