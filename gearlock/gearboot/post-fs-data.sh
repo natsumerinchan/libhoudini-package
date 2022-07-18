afbab09997c7148ae307f731f1152882860d03a6
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-18 17:15:41 +0800

    Update post-fs-data.sh

--- gearlock/gearboot/post-fs-data.sh
+++ gearlock/gearboot/post-fs-data.sh
@@ -4,14 +4,6 @@
 
 # Define your props inside a var in <prop> <value> format
 
-mount -o remount,rw /
-mount -o remount,rw /system
-if test -d "$SYSTEM_DIR/lib64/hw"; then
-	SYSTEM_ARCH=x86_64
-else
-	SYSTEM_ARCH=x86
-fi
-
 ARM_TRANSLATION_x86="
 ro.product.cpu.abilist x86,armeabi-v7a,armeabi
 ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
@@ -24,6 +16,7 @@ ro.enable.native.bridge.exec 1
 ro.dalvik.vm.isa.arm x86
 ro.zygote zygote32
 "
+
 ARM_TRANSLATION_x86_64="
 ro.product.cpu.abilist x86_64,x86,arm64-v8a,armeabi-v7a,armeabi
 ro.product.cpu.abilist32 x86,armeabi-v7a,armeabi
@@ -43,9 +36,6 @@ ro.zygote zygote64_32
 "
 
 # Now call the var over gearprop with quotes
+# You should modify 'gearprop "$ARM_TRANSLATION_x86_64"' to 'gearprop "$ARM_TRANSLATION_x86"' if your package architecture is x86.
 
-if test "$SYSTEM_ARCH" != "x86_64"; then
-        gearprop "$ARM_TRANSLATION_x86"
-else
-        gearprop "$ARM_TRANSLATION_x86_64"
-fi
+gearprop "$ARM_TRANSLATION_x86_64"
