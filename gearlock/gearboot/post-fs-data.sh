5abb0ddd5a25b68812167ea3b7a073e31766e52a
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-22 18:14:57 +0800

    Update
    houdini.rc comes from blissos

--- gearlock/gearboot/post-fs-data.sh
+++ gearlock/gearboot/post-fs-data.sh
@@ -1,3 +1,4 @@
+#!/gearlock/bin/bash
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
 # This script will be executed during post-fs-data stage.
 # You must delete this if you dont need it.
