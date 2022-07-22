6b33e04c2a7ad2ff4060159195e6ed1bc3b7b156
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-07-22 23:22:35 +0800

    Fix rootf devices after install magisk

--- gearlock/gearboot/init.sh
+++ gearlock/gearboot/init.sh
@@ -4,4 +4,11 @@
 # You must delete this if you dont need it.
 # Note: You don't need to manually deal with this file, GearLock will take care of it.
 
+# Enable native bridge for target executables
+
 mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+
+cp /system/etc/binfmt_misc/arm_exe /proc/sys/fs/binfmt_misc/register
+cp /system/etc/binfmt_misc/arm_dyn /proc/sys/fs/binfmt_misc/register
+cp /system/etc/binfmt_misc/arm64_exe /proc/sys/fs/binfmt_misc/register
+cp /system/etc/binfmt_misc/arm64_dyn /proc/sys/fs/binfmt_misc/register
