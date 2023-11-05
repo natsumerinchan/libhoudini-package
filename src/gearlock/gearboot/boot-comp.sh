b494300421c8bd68eff4cb8b9b649d349d949c75
セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>
2023-11-05 16:49:43 +0800

    Update boot-comp.sh

--- src/gearlock/gearboot/boot-comp.sh
+++ src/gearlock/gearboot/boot-comp.sh
@@ -5,13 +5,12 @@
 # Note: You don't need to manually deal with this file, GearLock will take care of it.
 
 regist_libhoudini() {
-    nsenter --mount=/proc/1/ns/mnt "mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc"
+    nsenter --mount=/proc/1/ns/mnt mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
 
-    nsenter --mount=/proc/1/ns/mnt "echo ':arm_exe:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x01\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x02\\\\x00\\\\x28::/system/bin/houdini:P' > /proc/sys/fs/binfmt_misc/register"
-    nsenter --mount=/proc/1/ns/mnt "echo ':arm_dyn:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x01\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x03\\\\x00\\\\x28::/system/bin/houdini:P' >> /proc/sys/fs/binfmt_misc/register"
-    nsenter --mount=/proc/1/ns/mnt "echo ':arm64_exe:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x02\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x02\\\\x00\\\\xb7::/system/bin/houdini64:P' >> /proc/sys/fs/binfmt_misc/register"
-    nsenter --mount=/proc/1/ns/mnt "echo ':arm64_dyn:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x02\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x03\\\\x00\\\\xb7::/system/bin/houdini64:P' >> /proc/sys/fs/binfmt_misc/register"
-    echo "Done."
+    nsenter --mount=/proc/1/ns/mnt echo ':arm_exe:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x01\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x02\\\\x00\\\\x28::/system/bin/houdini:P' > /proc/sys/fs/binfmt_misc/register
+    nsenter --mount=/proc/1/ns/mnt echo ':arm_dyn:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x01\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x03\\\\x00\\\\x28::/system/bin/houdini:P' >> /proc/sys/fs/binfmt_misc/register
+    nsenter --mount=/proc/1/ns/mnt echo ':arm64_exe:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x02\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x02\\\\x00\\\\xb7::/system/bin/houdini64:P' >> /proc/sys/fs/binfmt_misc/register
+    nsenter --mount=/proc/1/ns/mnt echo ':arm64_dyn:M::\\\\x7f\\\\x45\\\\x4c\\\\x46\\\\x02\\\\x01\\\\x01\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x00\\\\x03\\\\x00\\\\xb7::/system/bin/houdini64:P' >> /proc/sys/fs/binfmt_misc/register
 }
-
+echo "Hello World!" >> /data/adb/libhoudini/libhoudini.log
 regist_libhoudini 2>&1 | tee -a /data/adb/libhoudini/libhoudini.log
